dae044104374407b70f901b61170f32ab88a3e1d
Lukasz Raczylo <lukasz@raczylo.com>
2025-12-18 01:38:39 +0000

    fixup! fixup! fixup! fixup! More fixes, moving from python to golang worker.

--- workflow-prepare.sh
+++ workflow-prepare.sh
@@ -6,6 +6,9 @@ echo "Setting up envtest binaries..."
 # Install setup-envtest
 go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 
+# Add GOPATH/bin to PATH
+export PATH="${PATH}:$(go env GOPATH)/bin"
+
 # Download and setup envtest binaries
 ENVTEST_ASSETS=$(setup-envtest use 1.31.0 --bin-dir /tmp/envtest -p path)
 echo "KUBEBUILDER_ASSETS=${ENVTEST_ASSETS}" >> $GITHUB_ENV
